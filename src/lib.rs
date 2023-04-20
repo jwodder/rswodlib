f22b80407f361115a1fe029b337b4a3f91dcd66b
John T. Wodder II <git@varonathe.org>
2023-04-20 18:33:13 -0400

    Started out by writing a utility function that depends on nightly

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use std::str::pattern::Pattern;
+
+pub fn partition<'a, P: Pattern<'a>>(s: &str, pattern: P) -> Option<(&'a str, &'a str, &'a str)> {
+    let (i, sep) = s.match_indices(pattern).next()?;
+    Some((&s[..i], sep, &s[(i + sep.len())..]))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_partition_matches() {
+        assert_eq!(partition("abc-123-xyz", '-'), Some("abc", "-", "123-xyz"));
+    }
+
+    #[test]
+    fn test_partition_does_not_match() {
+        assert_eq!(partition("abc-123-xyz", ':'), None);
+    }
+
+    #[test]
+    fn test_partition_alternation_matches() {
+        assert_eq!(
+            partition("abc-123.xyz", ['.', '-']),
+            Some("abc", "-", "123.xyz")
+        );
+    }
+}
