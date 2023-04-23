b2673e2ec1dc5866e04b5ffd31e102159a450af7
John T. Wodder II <git@varonathe.org>
2023-04-23 16:48:11 -0400

    Added a `scan()` function

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,16 @@ pub fn partition<'a, P: Pattern<'a>>(
     Some((&s[..i], sep, &s[(i + sep.len())..]))
 }
 
+pub fn scan<P: FnMut(char) -> bool>(s: &str, mut predicate: P) -> (&str, &str) {
+    let boundary = s
+        .char_indices()
+        .take_while(move |&(_, ch)| predicate(ch))
+        .last()
+        .map(|(i, ch)| i + ch.len_utf8())
+        .unwrap_or_default();
+    s.split_at(boundary)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +40,20 @@ mod tests {
             partition("abc-123.xyz", ['.', '-']),
             Some(("abc", "-", "123.xyz"))
         );
+    }
+
+    #[test]
+    fn test_scan_half() {
+        assert_eq!(scan("123abc", |c| c.is_ascii_digit()), ("123", "abc"));
+    }
+
+    #[test]
+    fn test_scan_all() {
+        assert_eq!(scan("123456", |c| c.is_ascii_digit()), ("123456", ""));
+    }
+
+    #[test]
+    fn test_scan_none() {
+        assert_eq!(scan("abc123", |c| c.is_ascii_digit()), ("", "abc123"));
     }
 }
