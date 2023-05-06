da35b191755bbbf2ca7da8e083d27f94d12fb998
John T. Wodder II <git@varonathe.org>
2023-05-06 17:55:12 -0400

    rscan() and rscan_some()

---
+++ src/strings/rscan.rs
@@ -0,0 +1,42 @@
+/// Divides a string in two before the first character, counting from the
+/// right/end, that does not satisfy the given predicate and returns the two
+/// parts.  (Note that the second part is the maximal trailing substring of `s`
+/// whose characters all satisfy `predicate`.)
+///
+/// # Example
+///
+/// ```
+/// # use rswodlib::strings::rscan::rscan;
+/// assert_eq!(rscan("abc123", |c| c.is_ascii_digit()), ("abc", "123"));
+/// assert_eq!(rscan("123abc", |c| c.is_ascii_digit()), ("123abc", ""));
+/// ```
+pub fn rscan<P: FnMut(char) -> bool>(s: &str, mut predicate: P) -> (&str, &str) {
+    let boundary = s
+        .char_indices()
+        .rev()
+        .take_while(move |&(_, ch)| predicate(ch))
+        .last()
+        .map(|(i, _)| i)
+        .unwrap_or_else(|| s.len());
+    s.split_at(boundary)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn half() {
+        assert_eq!(rscan("abc123", |c| c.is_ascii_digit()), ("abc", "123"));
+    }
+
+    #[test]
+    fn all() {
+        assert_eq!(rscan("123456", |c| c.is_ascii_digit()), ("", "123456"));
+    }
+
+    #[test]
+    fn none() {
+        assert_eq!(rscan("123abc", |c| c.is_ascii_digit()), ("123abc", ""));
+    }
+}
---
+++ src/strings/rscan_some.rs
@@ -0,0 +1,47 @@
+/// Divides a string in two before the first character, counting from the
+/// right/end, that does not satisfy the given predicate.  If the second part
+/// is nonempty, the parts are returned.  Otherwise, `None` is returned.
+///
+/// Note that the second part is the maximal trailing substring of `s` whose
+/// characters all satisfy `predicate`.
+///
+/// # Example
+///
+/// ```
+/// # use rswodlib::strings::rscan_some::rscan_some;
+/// assert_eq!(rscan_some("abc123", |c| c.is_ascii_digit()), Some(("abc", "123")));
+/// assert_eq!(rscan_some("123abc", |c| c.is_ascii_digit()), None);
+/// ```
+pub fn rscan_some<P: FnMut(char) -> bool>(s: &str, mut predicate: P) -> Option<(&str, &str)> {
+    s.char_indices()
+        .rev()
+        .take_while(move |&(_, ch)| predicate(ch))
+        .last()
+        .map(|(i, _)| s.split_at(i))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn half() {
+        assert_eq!(
+            rscan_some("abc123", |c| c.is_ascii_digit()),
+            Some(("abc", "123"))
+        );
+    }
+
+    #[test]
+    fn all() {
+        assert_eq!(
+            rscan_some("123456", |c| c.is_ascii_digit()),
+            Some(("", "123456"))
+        );
+    }
+
+    #[test]
+    fn none() {
+        assert_eq!(rscan_some("123abc", |c| c.is_ascii_digit()), None);
+    }
+}
