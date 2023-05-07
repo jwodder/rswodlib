c00571a20ee408ba946d3d88e3a4a9bbb6364014
John T. Wodder II <git@varonathe.org>
2023-05-07 11:28:33 -0400

    Tests for runcmd.rs

--- src/runcmd.rs
+++ src/runcmd.rs
@@ -80,3 +80,120 @@ pub enum ReadcmdError {
     #[error("could not decode command output: {0}")]
     Decode(#[source] str::Utf8Error),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert_fs::prelude::*;
+    use assert_fs::NamedTempFile;
+    use predicates::prelude::*;
+
+    #[cfg(unix)]
+    #[test]
+    fn runcmd_bad_exit() {
+        let r = runcmd("sh", ["-c", "exit 42"]);
+        let Err(RuncmdError::Exit(rc)) = r else {
+            panic!("Command did not exit nonzero: {r:?}");
+        };
+        assert_eq!(rc.code(), Some(42));
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn runcmd_create_file() {
+        let tmpfile = NamedTempFile::new("content.txt").unwrap();
+        tmpfile.assert(predicate::path::missing());
+        runcmd(
+            "sh",
+            [
+                "-c",
+                &format!(
+                    "echo This is test text. > {}",
+                    tmpfile.path().to_str().expect("Tempfile path is not UTF-8")
+                ),
+            ],
+        )
+        .unwrap();
+        tmpfile.assert("This is test text.\n");
+    }
+
+    #[test]
+    fn runcmd_startup_failure() {
+        let r = runcmd::<[&str; 0], _>("this-command-does-not-exist", []);
+        let Err(RuncmdError::Startup(_)) = r else {
+            panic!("Command did not fail to start: {r:?}");
+        };
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn test_readcmd() {
+        let out = readcmd("printf", [r#"  This text will be stripped.\n\n"#]).unwrap();
+        assert_eq!(out, "This text will be stripped.");
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn readcmd_non_utf8() {
+        let r = readcmd("printf", [r#"The byte \200 is not valid UTF-8.\n"#]);
+        let Err(ReadcmdError::Decode(_)) = r else {
+            panic!("Command did not fail on decoding output: {r:?}");
+        };
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn readcmd_bad_exit() {
+        let r = readcmd(
+            "sh",
+            ["-c", r#"printf 'This will be discarded.\n'; exit 23"#],
+        );
+        let Err(ReadcmdError::Exit(rc)) = r else {
+            panic!("Command did not exit nonzero: {r:?}");
+        };
+        assert_eq!(rc.code(), Some(23));
+    }
+
+    #[test]
+    fn readcmd_startup_failure() {
+        let r = readcmd("nonexistent-echo", ["This", "is", "test", "text."]);
+        let Err(ReadcmdError::Startup(_)) = r else {
+            panic!("Command did not fail to start: {r:?}");
+        };
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn test_readcmd_lossy() {
+        let out = readcmd_lossy("printf", [r#"  This text will be stripped.\n\n"#]).unwrap();
+        assert_eq!(out, "This text will be stripped.");
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn readcmd_lossy_non_utf8() {
+        let out = readcmd_lossy("printf", [r#"The byte \200 is not valid UTF-8.\n"#]).unwrap();
+        assert_eq!(out, "The byte \u{FFFD} is not valid UTF-8.");
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn readcmd_lossy_bad_exit() {
+        let r = readcmd_lossy(
+            "sh",
+            ["-c", r#"printf 'This will be discarded.\n'; exit 23"#],
+        );
+        let Err(RuncmdError::Exit(rc)) = r else {
+            panic!("Command did not exit nonzero: {r:?}");
+        };
+        assert_eq!(rc.code(), Some(23));
+    }
+
+    #[test]
+    fn readcmd_lossy_startup_failure() {
+        let r = readcmd_lossy("nonexistent-echo", ["This", "is", "test", "text."]);
+        let Err(RuncmdError::Startup(_)) = r else {
+            panic!("Command did not fail to start: {r:?}");
+        };
+    }
+}
