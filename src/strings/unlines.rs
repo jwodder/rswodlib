ae66bb1573dd34cebc53042bec646e78ee1d073e
John T. Wodder II <git@varonathe.org>
2023-06-09 14:38:49 -0400

    unlines()

---
+++ src/strings/unlines.rs
@@ -0,0 +1,37 @@
+/// Return the concatenation of all elements of `iter` with a linefeed inserted
+/// after each element
+pub fn unlines<I, S>(iter: I) -> String
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<str>,
+{
+    let mut s = String::new();
+    for ln in iter {
+        s.push_str(ln.as_ref());
+        s.push('\n');
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        assert_eq!(unlines(std::iter::empty::<&str>()), "");
+    }
+
+    #[test]
+    fn one() {
+        assert_eq!(unlines(["foo"]), "foo\n");
+    }
+
+    #[test]
+    fn many() {
+        assert_eq!(
+            unlines(["foo", "bar", "", "baz\n", "quux"]),
+            "foo\nbar\n\nbaz\n\nquux\n"
+        );
+    }
+}
