8a4fe5d9a718e1c9b788a0a1880a02cfb41a9791
John T. Wodder II <git@varonathe.org>
2023-11-14 22:42:04 -0500

    Rewrite normalize_newlines() to use newlines()

--- src/strings/lines/normalize_newlines.rs
+++ src/strings/lines/normalize_newlines.rs
@@ -1,31 +1,22 @@
+use super::newlines::newlines;
 use std::borrow::Cow;
 
 /// Convert all CR LF and CR sequences in a string to LF
 pub fn normalize_newlines(s: &str) -> Cow<'_, str> {
     let mut buffer: Option<String> = None;
-    let mut cr_prev = false;
-    for (i, ch) in s.char_indices() {
-        if ch == '\r' {
-            if buffer.is_none() {
-                let mut b = String::with_capacity(s.len());
-                b.push_str(&s[..i]);
-                buffer = Some(b);
-            }
-            buffer
-                .as_mut()
-                .expect("buffer should be non-None")
-                .push('\n');
-            cr_prev = true;
-        } else {
-            if !(cr_prev && ch == '\n') {
-                if let Some(b) = buffer.as_mut() {
-                    b.push(ch);
-                }
-            }
-            cr_prev = false;
+    let mut cr_seen = false;
+    let mut i = 0;
+    for (start, end) in newlines(s) {
+        cr_seen |= &s[start..(start + 1)] == "\r";
+        if cr_seen {
+            let b = buffer.get_or_insert_with(|| String::with_capacity(s.len()));
+            b.push_str(&s[i..start]);
+            b.push('\n');
+            i = end;
         }
     }
-    if let Some(b) = buffer {
+    if let Some(mut b) = buffer {
+        b.push_str(&s[i..]);
         b.into()
     } else {
         s.into()
@@ -47,6 +38,12 @@ mod tests {
     #[case("foo\r\n", "foo\n", true)]
     #[case("foo\r\n\n", "foo\n\n", true)]
     #[case("foo\r", "foo\n", true)]
+    #[case("foo\nbar", "foo\nbar", false)]
+    #[case("foo\rbar", "foo\nbar", true)]
+    #[case("foo\r\nbar", "foo\nbar", true)]
+    #[case("foo\nbar\nbaz", "foo\nbar\nbaz", false)]
+    #[case("foo\rbar\rbaz", "foo\nbar\nbaz", true)]
+    #[case("foo\r\nbar\r\nbaz", "foo\nbar\nbaz", true)]
     #[case("foo\nbar\n", "foo\nbar\n", false)]
     #[case("foo\nbar\r\n", "foo\nbar\n", true)]
     #[case("foo\nbar\r", "foo\nbar\n", true)]
