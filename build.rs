60e9c85a98ff3259bb3c2c207d27ce21be77873e
John T. Wodder II <git@varonathe.org>
2023-11-22 14:50:07 -0500

    build.rs: Don't panic if version_meta fails

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    if version_meta().unwrap().channel == Channel::Nightly {
+    if version_meta().is_ok_and(|v| v.channel == Channel::Nightly) {
         println!("cargo:rustc-cfg=nightly");
     }
 }
