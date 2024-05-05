2c473b92c27a46e12ebbdd9552c46829b3d06685
John T. Wodder II <git@varonathe.org>
2024-05-05 12:00:24 -0400

    Delint

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use rustc_version::{version_meta, Channel};
 
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(nightly)");
     if version_meta().is_ok_and(|v| v.channel == Channel::Nightly) {
-        println!("cargo:rustc-cfg=nightly");
+        println!("cargo::rustc-cfg=nightly");
     }
 }
