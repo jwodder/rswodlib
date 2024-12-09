c7c837d6bdf414e60b501926142f2f2912865e97
John T. Wodder II <git@varonathe.org>
2024-12-09 07:51:21 -0500

    Rename `double_upto()` to `cross_upto()`

--- src/cross_upto.rs
+++ src/cross_upto.rs
@@ -6,8 +6,8 @@ use std::iter::FusedIterator;
 /// # Example
 ///
 /// ```
-/// # use rswodlib::double_upto::double_upto;
-/// let mut iter = double_upto(3, 2);
+/// # use rswodlib::cross_upto::cross_upto;
+/// let mut iter = cross_upto(3, 2);
 /// assert_eq!(iter.next(), Some((0, 0)));
 /// assert_eq!(iter.next(), Some((0, 1)));
 /// assert_eq!(iter.next(), Some((1, 0)));
@@ -16,21 +16,21 @@ use std::iter::FusedIterator;
 /// assert_eq!(iter.next(), Some((2, 1)));
 /// assert_eq!(iter.next(), None);
 /// ```
-pub fn double_upto(a: usize, b: usize) -> DoubleUpto {
-    DoubleUpto::new(a, b)
+pub fn cross_upto(a: usize, b: usize) -> CrossUpto {
+    CrossUpto::new(a, b)
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct DoubleUpto {
+pub struct CrossUpto {
     max_a: usize,
     max_b: usize,
     a: usize,
     b: usize,
 }
 
-impl DoubleUpto {
-    fn new(max_a: usize, max_b: usize) -> DoubleUpto {
-        DoubleUpto {
+impl CrossUpto {
+    fn new(max_a: usize, max_b: usize) -> CrossUpto {
+        CrossUpto {
             max_a,
             max_b,
             a: 0,
@@ -39,7 +39,7 @@ impl DoubleUpto {
     }
 }
 
-impl Iterator for DoubleUpto {
+impl Iterator for CrossUpto {
     type Item = (usize, usize);
 
     fn next(&mut self) -> Option<(usize, usize)> {
@@ -64,9 +64,9 @@ impl Iterator for DoubleUpto {
     }
 }
 
-impl FusedIterator for DoubleUpto {}
+impl FusedIterator for CrossUpto {}
 
-impl ExactSizeIterator for DoubleUpto {}
+impl ExactSizeIterator for CrossUpto {}
 
 #[cfg(test)]
 mod tests {
@@ -74,7 +74,7 @@ mod tests {
 
     #[test]
     fn test_3x2() {
-        let mut iter = double_upto(3, 2);
+        let mut iter = cross_upto(3, 2);
         assert_eq!(iter.size_hint(), (6, Some(6)));
         assert_eq!(iter.next(), Some((0, 0)));
         assert_eq!(iter.size_hint(), (5, Some(5)));
@@ -94,7 +94,7 @@ mod tests {
 
     #[test]
     fn test_0x2() {
-        let mut iter = double_upto(0, 2);
+        let mut iter = cross_upto(0, 2);
         assert_eq!(iter.size_hint(), (0, Some(0)));
         assert_eq!(iter.next(), None);
         assert_eq!(iter.next(), None);
@@ -102,7 +102,7 @@ mod tests {
 
     #[test]
     fn test_3x0() {
-        let mut iter = double_upto(3, 0);
+        let mut iter = cross_upto(3, 0);
         assert_eq!(iter.size_hint(), (0, Some(0)));
         assert_eq!(iter.next(), None);
         assert_eq!(iter.next(), None);
@@ -110,7 +110,7 @@ mod tests {
 
     #[test]
     fn test_0x0() {
-        let mut iter = double_upto(0, 0);
+        let mut iter = cross_upto(0, 0);
         assert_eq!(iter.size_hint(), (0, Some(0)));
         assert_eq!(iter.next(), None);
         assert_eq!(iter.next(), None);
