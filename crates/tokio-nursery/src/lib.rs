fb3edd02dc737535dee9cb608ab1f7c3aade2c9d
John T. Wodder II <git@varonathe.org>
2025-01-10 14:06:52 -0500

    Basic nursery tests

--- crates/tokio-nursery/src/lib.rs
+++ crates/tokio-nursery/src/lib.rs
@@ -47,7 +47,7 @@ pub struct NurseryStream<T> {
 impl<T: 'static> Stream for NurseryStream<T> {
     type Item = T;
 
-    /// Poll for one of the tasks in the group to complete and return its
+    /// Poll for one of the tasks in the nursery to complete and return its
     /// return value.
     ///
     /// # Panics
@@ -59,5 +59,62 @@ impl<T: 'static> Stream for NurseryStream<T> {
             Some(Err(e)) => std::panic::resume_unwind(e),
             None => None.into(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures_util::StreamExt;
+
+    #[test]
+    fn nursery_is_send() {
+        #[allow(dead_code)]
+        fn require_send<T: Send>(_t: T) {}
+
+        #[allow(dead_code)]
+        fn check_nursery_send<T: Send + 'static>() {
+            let (nursery, _) = Nursery::<T>::new();
+            require_send(nursery);
+        }
+    }
+
+    #[tokio::test]
+    async fn collect() {
+        let (nursery, nursery_stream) = Nursery::new();
+        nursery.spawn(std::future::ready(1));
+        nursery.spawn(std::future::ready(2));
+        nursery.spawn(std::future::ready(3));
+        drop(nursery);
+        let mut values = nursery_stream.collect::<Vec<_>>().await;
+        values.sort_unstable();
+        assert_eq!(values, vec![1, 2, 3]);
+    }
+
+    #[tokio::test]
+    async fn nested_spawn() {
+        let (nursery, nursery_stream) = Nursery::new();
+        let inner = nursery.clone();
+        nursery.spawn(async move {
+            inner.spawn(std::future::ready(0));
+            std::future::ready(1).await
+        });
+        nursery.spawn(std::future::ready(2));
+        nursery.spawn(std::future::ready(3));
+        drop(nursery);
+        let mut values = nursery_stream.collect::<Vec<_>>().await;
+        values.sort_unstable();
+        assert_eq!(values, vec![0, 1, 2, 3]);
+    }
+
+    #[tokio::test]
+    async fn reraise_panic() {
+        let (nursery, mut nursery_stream) = Nursery::new();
+        nursery.spawn(async { panic!("I can't take this anymore!") });
+        drop(nursery);
+        let r = std::panic::AssertUnwindSafe(nursery_stream.next())
+            .catch_unwind()
+            .await;
+        assert!(r.is_err());
     }
 }
