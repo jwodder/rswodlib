b9a699bfe579b8375946949f38b005c0a8c5c939
John T. Wodder II <git@varonathe.org>
2025-02-07 17:26:27 -0500

    tokio-worker-map: Doc comments

--- crates/tokio-worker-map/src/lib.rs
+++ crates/tokio-worker-map/src/lib.rs
@@ -8,6 +8,18 @@ use tokio::{select, task::JoinSet};
 
 type UnwindResult<T> = Result<T, Box<dyn std::any::Any + Send>>;
 
+/// Map values through asynchronous worker tasks.
+///
+/// `worker_map()` spawns `workers` concurrent tasks that loop over the values
+/// sent to the sender, apply `func` to them, and send the results to the
+/// receiver.  The sender is clonable, but the receiver is not.  If any
+/// application of `func` panics, the panic is reraised by the receiver.  Once
+/// the sender and all of its clones have been dropped, and once the results of
+/// all function applications have been received, the receiver stream will
+/// close.  If the receiver is dropped, the worker tasks are aborted.
+///
+/// `buffer_size` is the capacity of the the sender channel.  The receiver
+/// channel is unbounded.
 pub fn worker_map<F, Fut, T, U>(
     func: F,
     workers: NonZeroUsize,
@@ -59,38 +71,64 @@ where
     )
 }
 
+/// A handle for sending values to a [`worker_map()`] task.
+///
+/// Senders can be cloned and shared among threads. When all senders associated
+/// with a sender-receiver pair are dropped, the receiver becomes closed.
 #[derive(Debug)]
 pub struct Sender<T>(async_channel::Sender<T>);
 
 impl<T> Sender<T> {
+    /// Sends a task input into the channel, queuing it to be processed by the
+    /// next free worker task.
+    ///
+    /// If the channel is full, this method waits until there is space.
+    ///
+    /// If the channel is closed, this method returns an error.
     pub fn send(&self, msg: T) -> async_channel::Send<'_, T> {
         self.0.send(msg)
     }
 
+    /// Attempts to send a message into the channel.
+    ///
+    /// If the channel is full or closed, this method returns an error.
     pub fn try_send(&self, msg: T) -> Result<(), async_channel::TrySendError<T>> {
         self.0.try_send(msg)
     }
 
+    /// Closes the channel.  Any further calls to [`send()`](Self::send) or
+    /// [`try_send()`](Self::try_send) will return an error.
+    ///
+    /// Returns `true` if this call has closed the channel and it was not
+    /// closed already.
+    ///
+    /// Any pending task inputs will still be processed after calling
+    /// `close()`.
     pub fn close(&self) -> bool {
         self.0.close()
     }
 
+    /// Returns `true` if the channel is closed.
     pub fn is_closed(&self) -> bool {
         self.0.is_closed()
     }
 
+    /// Returns `true` if the channel is empty.
     pub fn is_empty(&self) -> bool {
         self.0.is_empty()
     }
 
+    /// Returns `true` if the channel is full.
     pub fn is_full(&self) -> bool {
         self.0.is_full()
     }
 
+    /// Returns the number of pending inputs in the channel.
     pub fn len(&self) -> usize {
         self.0.len()
     }
 
+    /// Returns the channel capacity.
     pub fn capacity(&self) -> usize {
         match self.0.capacity() {
             Some(n) => n,
@@ -107,9 +145,16 @@ impl<T> Clone for Sender<T> {
     }
 }
 
-// pin_project lets us call poll_recv() in poll_next() without even calling
+// pin_project! lets us call poll_recv() in poll_next() without even calling
 // project().  Not sure how.
 pin_project! {
+    /// A handle for receiving the results of operations from
+    /// [`worker_map()`] tasks.
+    ///
+    /// Receivers cannot be cloned.
+    ///
+    /// When a receiver is dropped, all corresponding worker tasks are aborted,
+    /// and the sender channels are closed.
     #[derive(Debug)]
     pub struct Receiver<T, U> {
         inner: tokio::sync::mpsc::UnboundedReceiver<UnwindResult<U>>,
@@ -120,6 +165,18 @@ pin_project! {
 }
 
 impl<T: Send, U: Send> Receiver<T, U> {
+    /// Receives a result from a worker task.
+    ///
+    /// If the channel is empty, this method waits until there is a message.
+    ///
+    /// If the channel is closed (due to either all senders having been dropped
+    /// or `close()` having been called), this method receives a result or
+    /// returns `None` if there are no more results.
+    ///
+    /// # Panics
+    ///
+    /// If the channel receives a result from a task that panicked, this method
+    /// resumes unwinding the panic.
     pub async fn recv(&mut self) -> Option<U> {
         match self.inner.recv().await? {
             Ok(r) => Some(r),
@@ -129,15 +186,29 @@ impl<T: Send, U: Send> Receiver<T, U> {
 }
 
 impl<T, U> Receiver<T, U> {
+    /// Closes the corresponding [`Sender`]s.  Any further calls to
+    /// [`Sender::send()`] or [`Sender::try_send()`] will return an error.
+    ///
+    /// Returns `true` if this call has closed the senders and they were not
+    /// closed already.
+    ///
+    /// Any pending task inputs will still be processed after calling
+    /// `close()`.
     pub fn close(&self) -> bool {
         self.closer.close()
     }
 
+    /// Returns `true` if the sender/receiver is closed.
     pub fn is_closed(&self) -> bool {
         self.closer.is_closed()
     }
 
-    // Returns `true` if `shutdown()` was not already called before
+    /// Calls [`close()`](Self::close) and additionally instructs the worker
+    /// tasks to not process any pending task inputs.  Any inputs currently
+    /// being processed are still processed to completion.
+    ///
+    /// Returns `true` if this call has shut down the sender/receiver and it
+    /// was not shut down already.
     pub fn shutdown(&self) -> bool {
         self.close();
         self.shutdown_sender.send_if_modified(|b| {
@@ -150,18 +221,35 @@ impl<T, U> Receiver<T, U> {
         })
     }
 
+    /// Returns `true` if the sender/receiver is shut down.
     pub fn is_shutdown(&self) -> bool {
         *self.shutdown_sender.borrow()
     }
 
+    /// Returns `true` if the channel is empty.
     pub fn is_empty(&self) -> bool {
         self.inner.is_empty()
     }
 
+    /// Returns the number of pending outputs in the channel.
     pub fn len(&self) -> usize {
         self.inner.len()
     }
 
+    /// Polls to receive the next result on this channel.
+    ///
+    /// This method returns:
+    ///
+    ///  * `Poll::Pending` if no results are available but the channel is not
+    ///    closed, or if a spurious failure happens.
+    ///  * `Poll::Ready(Some(message))` if a result is available.
+    ///  * `Poll::Ready(None)` if the channel has been closed and all results
+    ///    sent before it was closed have been received.
+    ///
+    /// # Panics
+    ///
+    /// If the channel receives a result from a task that panicked, this method
+    /// resumes unwinding the panic.
     pub fn poll_recv(&mut self, cx: &mut Context<'_>) -> Poll<Option<U>> {
         match ready!(self.inner.poll_recv(cx)) {
             Some(Ok(r)) => Some(r).into(),
@@ -174,6 +262,13 @@ impl<T, U> Receiver<T, U> {
 impl<T, U: 'static> Stream for Receiver<T, U> {
     type Item = U;
 
+    /// Poll for one of the worker tasks to finish processing an input value,
+    /// and return the output.
+    ///
+    /// # Panics
+    ///
+    /// If the channel receives a result from a task that panicked, this method
+    /// resumes unwinding the panic.
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<U>> {
         self.poll_recv(cx)
     }
